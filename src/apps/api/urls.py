441e6ab5c403b07e991d52668fa9f47ca7164989
Tsotetsi <me@tsotetsithapelo.co.za>
2022-08-27 19:51:57 +0200

    Fix typo.

--- src/apps/api/urls.py
+++ src/apps/api/urls.py
@@ -4,7 +4,7 @@ from rest_framework import routers
 from .views import UserViewSet
 
 router = routers.DefaultRouter()
-router.register(r'^users', UserViewSet, basename='users')
+router.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
     re_path(r'^', include(router.urls))
